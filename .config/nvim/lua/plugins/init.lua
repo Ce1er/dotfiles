168a64d1f475f52926a7b37ccd6647f2a5927711
Ce1er <114954714+Ce1er@users.noreply.github.com>
2024-10-19 18:10:17 +0100

    Add files via upload
    Alphabetically sorted plugins/init.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,23 +1,13 @@
 return {
   require "configs.conform",
-
-  require "configs.lspconfig",
-
-  require "configs.telescope-dap",
-
   require "configs.dap",
-
-  require "configs.dapui",
-
   require "configs.dap-virtual-text",
-
-  require "configs.nio",
-
-  require "configs.treesitter",
-
-  require "configs.overseer",
-
-  require "configs.toggleterm",
-
+  require "configs.dapui",
   require "configs.lint",
+  require "configs.lspconfig",
+  require "configs.nio",
+  require "configs.overseer",
+  require "configs.telescope-dap",
+  require "configs.toggleterm",
+  require "configs.treesitter",
 }
