5ec4623b65fb35f92e3cf1bbd97c2b1e2b16fb59
Ce1er <114954714+Ce1er@users.noreply.github.com>
2024-10-28 23:33:38 +0000

    Add files via upload

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -6,5 +6,5 @@ swww img -o DP-1 --transition-fps 240 --transition-type grow --transition-pos bo
 if [ $(pgrep wayvnc) != ""]; then
     swww img -o HEADLESS --transition-step 255 $wallpaper
 fi
-wal -i "$(swww query | head -n 1 | awk '{ print $8 }')"
+wal -n -i "$(swww query | head -n 1 | awk '{ print $8 }')"
 polychromatic-cli -d keyboard --zone main -c $(sed '2q;d' ~/.cache/wal/colors) -o static # In sed command number before q is the line to use
--- .config/hypr/waybar-restart.sh
+++ .config/hypr/waybar-restart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+~/update_waybar_colors.sh
 killall waybar
 waybar &
